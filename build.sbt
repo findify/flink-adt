ea0548dddc82ac180d24bca366951783ba710eb9
Nick <nick.burkard@gmail.com>
2022-01-28 17:08:27 -0500

    fully drop flink-scala, use flink 1.15

--- build.sbt
+++ build.sbt
@@ -12,15 +12,18 @@ publishMavenStyle := true
 
 publishTo := sonatypePublishToBundle.value
 
-lazy val flinkVersion = "1.14.0"
+// Use snapshot of Flink 1.15 for now.
+// Otherwise impossible to import `flink-test-utils` without transitive Scala dependency.
+resolvers += "Apache Snapshots" at "https://repository.apache.org/content/repositories/snapshots/"
+lazy val flinkVersion = "1.15-20220128.014046-92"
 
 libraryDependencies ++= Seq(
-  "com.softwaremill.magnolia1_2" % "magnolia_2.12"         % "1.0.0-M7",
-  "org.apache.flink"            %% "flink-scala"           % flinkVersion % "provided",
-  "org.apache.flink"            %% "flink-streaming-scala" % flinkVersion % "provided",
-  "org.apache.flink"            %% "flink-test-utils"      % flinkVersion % "test",
-  "org.scalatest"               %% "scalatest"             % "3.2.10"     % "test",
-  "org.typelevel"               %% "cats-core"             % "2.7.0"      % "test"
+  "com.softwaremill.magnolia1_2" %% "magnolia" % "1.0.0-M8",
+  "org.apache.flink" % "flink-java" % flinkVersion % "provided",
+  "org.apache.flink" % "flink-test-utils" % flinkVersion % "test",
+  "org.scalatest" %% "scalatest" % "3.2.10" % "test",
+  "org.typelevel" %% "cats-core" % "2.7.0" % "test",
+  "org.scala-lang" % "scala-reflect" % scalaVersion.value
 )
 
 scmInfo := Some(
--- src/test/scala/io/findify/flinkadt/ExampleTest.scala
+++ src/test/scala/io/findify/flinkadt/ExampleTest.scala
@@ -5,11 +5,13 @@ import org.apache.flink.api.common.RuntimeExecutionMode
 import org.apache.flink.api.common.restartstrategy.RestartStrategies
 import org.apache.flink.api.common.typeinfo.TypeInformation
 import org.apache.flink.runtime.testutils.MiniClusterResourceConfiguration
-import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
 import org.apache.flink.test.util.MiniClusterWithClientResource
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+
+import scala.collection.JavaConverters._
 
 class ExampleTest extends AnyFlatSpec with Matchers with BeforeAndAfterAll {
   lazy val cluster = new MiniClusterWithClientResource(
@@ -41,14 +43,13 @@ class ExampleTest extends AnyFlatSpec with Matchers with BeforeAndAfterAll {
     import io.findify.flinkadt.api._
 
     implicit val eventTypeInfo: TypeInformation[Event] = deriveTypeInformation[Event]
-    val result = env.fromCollection(List[Event](Click("1"), Purchase(1.0))).executeAndCollect(10)
+    val result = env.fromCollection(List(Click("1"), Purchase(1.0)).asJava, eventTypeInfo).executeAndCollect(10)
     result.size shouldBe 2
   }
 }
 
 object ExampleTest {
-  sealed trait Event
-  case class Click(id: String)       extends Event
-  case class Purchase(price: Double) extends Event
-
+  sealed trait Event extends Product with Serializable
+  final case class Click(id: String) extends Event
+  final case class Purchase(price: Double) extends Event
 }
